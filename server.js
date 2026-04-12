1c3bfbcbc4730609104a27f9b013a1ff258854d4
jushuaSol3 <nikokushiro3@gmail.com>
2026-04-12 16:20:55 +0800

    fix the backend

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+require('dotenv').config();
 const path = require('path');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
