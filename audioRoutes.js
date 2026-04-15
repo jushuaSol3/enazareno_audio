aa53ab4dbf7b84a86fd9d9d2eb06766f5e8e80e3
jushuaSol3 <nikokushiro3@gmail.com>
2026-04-16 00:04:53 +0800

    back to default

--- audioRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPrefaceAudio, getStoryAudio, getTest } = require('./audioController');
-
-router.get('/audio/:bookId', getPrefaceAudio);
-router.get('/audio/:bookId/story', getStoryAudio);
-router.get('/test', getTest);
-
-module.exports = router;
